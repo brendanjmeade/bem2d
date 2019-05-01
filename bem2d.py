Some more work on traction partials.  Result is still wrong when applied to half-space calculation

bem2d.py
@@ -1872,13 +1872,28 @@ def constant_partials_single(element_obs, element_src, mu, nu):
     partials_stress = np.zeros((3, 2))
     partials_stress[:, 0::2] = stress_strike_slip
     partials_stress[:, 1::2] = stress_tensile_slip
+    partials_traction = np.zeros((2, 2))
 
-    partials_stress = np.zeros((3, 2))
-    partials_stress[:, 0::2] = stress_strike_slip
-    partials_stress[:, 1::2] = stress_tensile_slip
+    normal_vector = np.array([element_obs["x_normal"], element_obs["y_normal"]])
+    stress_tensor_strike_slip = np.array(
+        [
+            [stress_strike_slip[0], stress_strike_slip[2]],
+            [stress_strike_slip[2], stress_strike_slip[1]],
+        ]
+    )
+    stress_tensor_tensile_slip = np.array(
+        [
+            [stress_tensile_slip[0], stress_tensile_slip[2]],
+            [stress_tensile_slip[2], stress_tensile_slip[1]],
+        ]
+    )
 
-
-    return partials_displacement, partials_stress
+    traction_strike_slip = stress_tensor_strike_slip[:, :, 0] @ normal_vector
+    traction_tensile_slip = stress_tensor_tensile_slip[:, :, 0] @ normal_vector
+    # import ipdb; ipdb.set_trace()
+    partials_traction[:, 0::2] = traction_strike_slip[:, np.newaxis]
+    partials_traction[:, 1::2] = traction_tensile_slip[:, np.newaxis]
+    return partials_displacement, partials_stress, partials_traction
 
 
 def constant_partials_all(elements_obs, elements_src, mu, nu):
@@ -1888,14 +1903,14 @@ def constant_partials_all(elements_obs, elements_src, mu, nu):
     stride = 2  # number of columns per element
     partials_displacement = np.zeros((stride * n_elements_obs, stride * n_elements_src))
     partials_stress = np.zeros((3 * n_elements_obs, stride * n_elements_src))
+    partials_traction = np.zeros((stride * n_elements_obs, stride * n_elements_src))
     idx_obs = stride * np.arange(n_elements_obs + 1)
     idx_src = stride * np.arange(n_elements_src + 1)
-    stress_idx_obs = 3 * np.arange(n_elements_src + 1)
-    # stress_idx_src = 3 * np.arange(n_elements_src + 1) # Can I really delete this???
+    stress_idx_obs = 3 * np.arange(n_elements_obs + 1)
 
     for i_src, element_src in enumerate(elements_src):
         for i_obs, element_obs in enumerate(elements_obs):
-            displacement, stress = constant_partials_single(
+            displacement, stress, traction = constant_partials_single(
                 element_obs, element_src, mu, nu
             )
             partials_displacement[
@@ -1905,27 +1920,10 @@ def constant_partials_all(elements_obs, elements_src, mu, nu):
                 stress_idx_obs[i_obs] : stress_idx_obs[i_obs + 1],
                 idx_src[i_src] : idx_src[i_src + 1],
             ] = stress
-    return partials_displacement, partials_stress
-
-
-def stress_to_traction(elements, stress):
-    """ Convert stresses to tractions """
-    traction = np.zeros(displacement.shape)
-    for i, element in enumerate(elements):
-        normal_vector_obs = np.array(
-            [element["x_normal"], element["y_normal"]]
-        )
-
-        # I feel like this is wrong!!!
-        stress_tensor_obs = np.array(
-            [[stress[0, j], stress[2, j]], [stress[2, j], stress[1, j]]]
-        )
-        traction[0, j], traction[1, j] = np.dot(
-            stress_tensor_obs, normal_vector_obs
-        )
-    traction_partials[0::2, (2 * i) + 1] = traction[0, :]
-    traction_partials[1::2, (2 * i) + 1] = traction[1, :]
-    return traction
+            partials_traction[
+                idx_obs[i_obs] : idx_obs[i_obs + 1], idx_src[i_src] : idx_src[i_src + 1]
+            ] = traction
+    return partials_displacement, partials_stress, partials_traction
 
 
 def quadratic_partials_single(element_obs, element_src, mu, nu):

example_thrust_free_surface.py
@@ -25,9 +25,16 @@ element = {}
 
 
 def analytic(x):
-    delta = np.deg2rad(135)
+    # fault dipping at 45 degrees
+    # delta = np.deg2rad(135)
+    # depth = 1
+    # xi = -1
+
+    # fault dipping at 90 degrees
+    delta = np.deg2rad(90)
     depth = 1
-    xi = -1
+    xi = 0
+
     zeta = (x - xi) / depth
     ux = (
         1
@@ -59,7 +66,8 @@ for i in range(0, x1.size):
 elements_surface = bem2d.standardize_elements(elements_surface)
 
 # Constant slip fault
-x1, y1, x2, y2 = bem2d.discretized_line(-1, -1, 0, 0, 10)
+# x1, y1, x2, y2 = bem2d.discretized_line(-1, -1, 0, 0, 10)
+x1, y1, x2, y2 = bem2d.discretized_line(0, -1, 0, 0, 10)
 for i in range(0, x1.size):
     element["x1"] = x1[i]
     element["y1"] = y1[i]
@@ -68,25 +76,16 @@ for i in range(0, x1.size):
     elements_fault.append(element.copy())
 elements_fault = bem2d.standardize_elements(elements_fault)
 
-
-# Build partial derivative matrices for Ben's thrust fault problem
-displacement_partials_1, traction_partials_1 = bem2d.constant_linear_partials(
-    elements_fault, elements_surface, "slip", mu, nu
-)
-
-displacement_partials_2, traction_partials_2 = bem2d.constant_linear_partials(
-    elements_surface, elements_surface, "slip", mu, nu
-)
+d1, s1, t1 = bem2d.constant_partials_all(elements_surface, elements_fault, mu, nu)
+d2, s2, t2 = bem2d.constant_partials_all(elements_surface, elements_surface, mu, nu)
 
 # Predict surface displacements from unit strike slip forcing
 x_center = np.array([_["x_center"] for _ in elements_surface])
 fault_slip = np.zeros(2 * len(elements_fault))
-fault_slip[1::2] = -1.0
+fault_slip[0::2] = -1.0
 
-disp_full_space = displacement_partials_1 @ fault_slip
-disp_free_surface = np.linalg.inv(traction_partials_2) @ (
-    traction_partials_1 @ fault_slip
-)
+disp_full_space = d1 @ fault_slip
+disp_free_surface = np.linalg.inv(t2) @ (t1 @ fault_slip)
 disp_free_surface_analytic = analytic(x_center)
 
 plt.figure()
@@ -119,18 +118,16 @@ plt.title("u_y")
 plt.legend(["half space", "full space"])
 plt.tight_layout()
 
-full_mat = np.linalg.inv(traction_partials_2) @ (traction_partials_1)
+full_mat = np.linalg.inv(t2) @ t1
 fm2 = full_mat.reshape((full_mat.shape[0] // 2, 2, full_mat.shape[1] // 2, 2))
 fm3 = np.swapaxes(np.swapaxes(fm2, 0, 1), 2, 3).reshape(full_mat.shape)
 plt.matshow(fm3)
-plt.title("surface - surface")
-
+plt.title("partials: surface - surface")
 plt.matshow(np.log10(np.abs(fm3)))
-plt.title("fault - surface")
+plt.title("partials: fault - surface")
 
 plt.show(block=False)
 
-d, s = bem2d.constant_partials_all(elements_surface, elements_surface, mu, nu)
 
 # # Predict internal displacements everywhere
 # fault_slip_ss = fault_slip[0::2]