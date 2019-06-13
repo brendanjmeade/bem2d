ba25f6a393743f7ad92fe0f5c2ebd2e6ea4d20fc
brendanjmeade <brendanjmeade@gmail.com>
2019-06-13 18:32:23 -0400

    Finished moving model parameters to PARAMETERS dictionary

--- example_planar_rupture_quadratic.py
+++ example_planar_rupture_quadratic.py
@@ -12,56 +12,58 @@ import bem2d.newton_rate_state
 bem2d = reload(bem2d)
 plt.close("all")
 
-# TODO: Work through vector block motion for non-planar
 # TODO: Try inclined fault
-# TODO: Material parameters for each fault element
-# TODO: Group other parameters into dictionary
+# TODO: Figure out how to enter boundary condition better?
 
-# DAE solver parameters
-TOL = 1e-12
+# Constants and model parameters
+NEWTON_TOL = 1e-12
 MAXITER = 50
+ODE_ATOL = 1e-4
+ODE_RTOL = 1e-4
+N_NODES_PER_ELEMENT = 3
+SPY = 365 * 24 * 60 * 60  # Seconds per year
+TIME_INTERVAL = SPY * np.array([0.0, 600.0])
+PARAMETERS = {}
+PARAMETERS["mu"] = 3e10
+PARAMETERS["nu"] = 0.25
+PARAMETERS["density"] = 2700  # rock density (kg/m^3)
+PARAMETERS["cs"] = np.sqrt(
+    PARAMETERS["mu"] / PARAMETERS["density"]
+)  # Shear wave speed (m/s)
+PARAMETERS["eta"] = PARAMETERS["mu"] / (
+    2 * PARAMETERS["cs"]
+)  # Radiation damping coefficient (kg / (m^2 * s))
+PARAMETERS["d_c"] = 0.05  # state evolution length scale (m)
+PARAMETERS["f_0"] = 0.6  # baseline coefficient of friction
+PARAMETERS["block_velocity_x"] = 1e-9
+PARAMETERS["block_velocity_y"] = 0
+PARAMETERS["v_0"] = 1e-6 # reference velocity
 
-# Material properties
-mu = 3e10  # Shear modulus (Pa)
-nu = 0.25  # Possion's ratio
-density = 2700  # rock density (kg/m^3)
-cs = np.sqrt(mu / density)  # Shear wave speed (m/s)
-eta = mu / (2 * cs)  # The radiation damping coefficient (kg / (m^2 * s))
-Dc = 0.05  # state evolution length scale (m)
-f0 = 0.6  # baseline coefficient of friction
-
-# Unique to each fault segment
-Vp = 1e-9  # Rate of plate motion
-sigma_n = 50e6  # Normal stress (Pa)
-a = 0.015  # direct velocity strengthening effect
-b = 0.02  # state-based velocity weakening effect
-v_0 = 1e-6  # when V = V0, f = f0, V is (m/s)
-secs_per_year = 365 * 24 * 60 * 60
-time_interval_yrs = np.linspace(0.0, 600.0, 5001)
-time_interval = time_interval_yrs * secs_per_year
-
-# Crete fault elements
+# Create fault elements
+N_ELEMENTS = 50
+N_NODES = 3 * N_ELEMENTS
 ELEMENTS_FAULT = []
 ELEMENT = {}
 L = 10000
-x1, y1, x2, y2 = bem2d.discretized_line(-L, 0, L, 0, 50)
+x1, y1, x2, y2 = bem2d.discretized_line(-L, 0, L, 0, N_ELEMENTS)
 for i in range(0, x1.size):
     ELEMENT["x1"] = x1[i]
     ELEMENT["y1"] = y1[i]
     ELEMENT["x2"] = x2[i]
     ELEMENT["y2"] = y2[i]
-    ELEMENT["a"] = a
-    ELEMENT["b"] = b
-    ELEMENT["sigma_n"] = sigma_n
+    ELEMENT["a"] = 0.015  # frictional a parameter ()
+    ELEMENT["b"] = 0.020  # frictional b parameter ()
+    ELEMENT["sigma_n"] = 50e6  # normal stress (Pa)
     ELEMENTS_FAULT.append(ELEMENT.copy())
 ELEMENTS_FAULT = bem2d.standardize_elements(ELEMENTS_FAULT)
-N_ELEMENTS = len(ELEMENTS_FAULT)
-N_NODES = 3 * N_ELEMENTS
 
 # Extract parameters for later use as arrays
 ELEMENTS_FAULT_ARRAYS = {}
 ELEMENTS_FAULT_ARRAYS["a"] = np.array(
     [np.tile(_["a"], 3) for _ in ELEMENTS_FAULT]
+).flatten()
+ELEMENTS_FAULT_ARRAYS["b"] = np.array(
+    [np.tile(_["b"], 3) for _ in ELEMENTS_FAULT]
 ).flatten()
 ELEMENTS_FAULT_ARRAYS["additional_normal_stress"] = np.array(
     [np.tile(_["sigma_n"], 3) for _ in ELEMENTS_FAULT]
@@ -75,17 +77,28 @@ ELEMENTS_FAULT_ARRAYS["element_normals"][:, 1] = np.array(
 ).flatten()
 
 # Calculate slip to traction partials on the fault
+START_TIME = time.time()
 _, _, SLIP_TO_TRACTION = bem2d.quadratic_partials_all(
-    ELEMENTS_FAULT, ELEMENTS_FAULT, mu, nu
+    ELEMENTS_FAULT, ELEMENTS_FAULT, PARAMETERS["mu"], PARAMETERS["nu"]
 )
+PARTIALS_TIME = time.time() - START_TIME
 
 
 def calc_state(state, velocity):
     """ State evolution law : aging law """
-    return b * v_0 / Dc * (np.exp((f0 - state) / b) - (velocity / v_0))
+    state = (
+        ELEMENTS_FAULT_ARRAYS["b"]
+        * PARAMETERS["v_0"]
+        / PARAMETERS["d_c"]
+        * (
+            np.exp((PARAMETERS["f_0"] - state) / ELEMENTS_FAULT_ARRAYS["b"])
+            - (velocity / PARAMETERS["v_0"])
+        )
+    )
+    return state
 
 
-def steady_state(velocities):  # Can I vectorize this?
+def steady_state(velocities):  # Should I eliminate this since its only called once?
     """ Steady-state state for initial condition """
     state = scipy.optimize.fsolve(calc_state, 0.5 * np.ones(N_NODES), args=(velocities))
     return state
@@ -94,7 +107,7 @@ def steady_state(velocities):  # Can I vectorize this?
 def calc_derivatives(t, x_and_state):
     """ Derivatives to feed to ODE integrator """
     state = x_and_state[2::3]
-    x = np.zeros(2 * state.size)
+    x = np.empty(2 * N_NODES)
     x[0::2] = x_and_state[0::3]
     x[1::2] = x_and_state[1::3]
 
@@ -109,20 +122,21 @@ def calc_derivatives(t, x_and_state):
         state,
         current_velocity,  # Modified in place and returns velocity solution
         ELEMENTS_FAULT_ARRAYS["a"],
-        eta,
-        v_0,
+        PARAMETERS["eta"],
+        PARAMETERS["v_0"],
         0.0,
         ELEMENTS_FAULT_ARRAYS["additional_normal_stress"],
-        TOL,
+        NEWTON_TOL,
         MAXITER,
-        3,
+        N_NODES_PER_ELEMENT,
     )
 
     dx_dt = -current_velocity  # Is the negative sign for slip deficit convention?
-    dx_dt[0::2] += Vp  # FIX TO USE Vp in the plate direction
-    vel_mags = np.linalg.norm(current_velocity.reshape((-1, 2)), axis=1)
-    dstate_dt = calc_state(state, vel_mags)
-    derivatives = np.zeros(dx_dt.size + dstate_dt.size)
+    dx_dt[0::2] += PARAMETERS["block_velocity_x"]
+    dx_dt[1::2] += PARAMETERS["block_velocity_y"]
+    velocity_magnitude = np.linalg.norm(current_velocity.reshape((-1, 2)), axis=1)
+    dstate_dt = calc_state(state, velocity_magnitude)
+    derivatives = np.empty(3 * N_NODES)
     derivatives[0::3] = dx_dt[0::2]
     derivatives[1::3] = dx_dt[1::2]
     derivatives[2::3] = dstate_dt
@@ -130,38 +144,46 @@ def calc_derivatives(t, x_and_state):
 
 
 # Set initial conditions and time integrate
-initial_velocity_x = Vp / 1000.0 * np.ones(N_NODES)
-initial_velocity_y = 0.0 * np.ones(N_NODES)
-initial_conditions = np.zeros(3 * N_NODES)
-initial_conditions[0::3] = initial_velocity_x
-initial_conditions[1::3] = initial_velocity_y
-initial_conditions[2::3] = steady_state(initial_velocity_x)
+initial_velocity_x = 1e-3 * PARAMETERS["block_velocity_x"] * np.ones(N_NODES)
+initial_velocity_y = 1e-3 * PARAMETERS["block_velocity_y"] * np.ones(N_NODES)
+INITIAL_CONDITIONS = np.empty(3 * N_NODES)
+INITIAL_CONDITIONS[0::3] = initial_velocity_x
+INITIAL_CONDITIONS[1::3] = initial_velocity_y
+INITIAL_CONDITIONS[2::3] = steady_state(initial_velocity_x) # Change to velocity magnitude
+
 
 SOLVER = scipy.integrate.RK23(
     calc_derivatives,
-    time_interval.min(),
-    initial_conditions,
-    time_interval.max(),
-    rtol=1e-4,
-    atol=1e-4,
+    TIME_INTERVAL.min(),
+    INITIAL_CONDITIONS,
+    TIME_INTERVAL.max(),
+    rtol=ODE_RTOL,
+    atol=ODE_ATOL,
+    vectorized=False,
 )
 
+START_TIME = time.time()
 SOLUTION = {}
 SOLUTION["t"] = [SOLVER.t]
 SOLUTION["y"] = [SOLVER.y.copy()]
-while SOLVER.t < time_interval.max():
+while SOLVER.t < TIME_INTERVAL.max():
     SOLVER.step()
+    N_STEP = len(SOLUTION["t"])
     print(
-        f"t = {SOLVER.t / secs_per_year:05.6f}"
+        f"t = {SOLVER.t / SPY:05.6f}"
         + " of "
-        + f"{time_interval.max() / secs_per_year:05.6f}"
-        + f" ({100 * SOLVER.t / time_interval.max():.3f}"
+        + f"{TIME_INTERVAL.max() / SPY:05.6f}"
+        + f" ({100 * SOLVER.t / TIME_INTERVAL.max():.3f}"
         + "%)"
+        + f" {N_STEP} steps"
     )
     SOLUTION["t"].append(SOLVER.t)
     SOLUTION["y"].append(SOLVER.y.copy())
 SOLUTION["t"] = np.array(SOLUTION["t"])
 SOLUTION["y"] = np.array(SOLUTION["y"])
+SOLVER_TIME = time.time() - START_TIME
+print("Partials time: " + f"{PARTIALS_TIME:.2f}" + " (seconds)")
+print("Solver time: " + f"{SOLVER_TIME:.2f}" + " (seconds)")
 
 
 def plot_time_series():
@@ -170,7 +192,7 @@ def plot_time_series():
     y_labels = ["$u_x$ (m)", "$u_y$ (m)", "state"]
     for i, y_label in enumerate(y_labels):
         plt.subplot(3, 1, i + 1)
-        plt.plot(SOLUTION["t"] / secs_per_year, SOLUTION["y"][:, i::3], linewidth=0.5)
+        plt.plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, i::3], linewidth=0.5)
         plt.ylabel(y_label)
     plt.xlabel("$t$ (years)")
     plt.show(block=False)
